cf41f27060e7a8865691cb3ef451d79e21ee1e70
Sol <solmseda@gmail.com>
2020-04-02 14:31:43 -0300

    Criei um script para controlar a cena e uma pasta de prefabs
    No script SceneControl tem os comandos para pausar, sair, reiniciar e resumir. Também criei um prefab da tela de pausa e coloquei em uma pasta de prefabs

---
+++ Time03/Assets/Scripts/SceneControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    public GameObject PauseMenuUI;
+    public bool GameIsPaused = false;
+
+    void Start()
+    {
+        PauseMenuUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(GameIsPaused){
+                Resume();
+            }
+            else if(!GameIsPaused){
+                Pause();
+            }
+        }
+    }
+
+    public void Pause(){
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume(){
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void ChangeScene(string SceneName){
+        SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+    }
+
+    public void ReloadScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
