487519e50920fad2ed683071a43c4e257b298234
Nagib Suaid <nagibmoura@gmail.com>
2020-04-08 13:22:27 -0300

    array de Skills no carinho

--- Time03/Assets/Scripts/CarinhoScript.cs
+++ Time03/Assets/Scripts/CarinhoScript.cs
@@ -5,10 +5,26 @@ using UnityEngine;
 public class CarinhoScript : MonoBehaviour
 {
     public Transform PlayerPosition;
+
     private Rigidbody _rb;
-        void Start()
+
+    private Skills[] skills;
+
+    public float orbitCd;
+    public float orbitProb;
+
+    public float launchCd;
+    public float launchProb;
+
+    public float bombCd;
+    public float bombProb;
+
+    void Start()
     {
-        
+        skills = new Skills[3];
+        skills[0] = new Skills(orbitProb, orbitCd, false);
+        skills[1] = new Skills(launchProb, launchCd, false);
+        skills[2] = new Skills(bombProb, bombCd, false);
     }
 
     // Update is called once per frame
