280bbb2988e0aec1f24805cdddd1576b35d3246f
Arthur Ozorio <arthurlozorio@globo.com>
2020-04-09 15:40:22 -0300

    Heart Orbit

--- Time03/Assets/Scripts/CarinhoScript.cs
+++ Time03/Assets/Scripts/CarinhoScript.cs
@@ -9,16 +9,14 @@ public class CarinhoScript : MonoBehaviour
     public float CoolDownLaunch;
     public float lookRadius = 10f; // se o jogador entrar nesse raio o carinho vai começar a perseguir ele
     public Transform[] carinhoHearts;
-
-    private Skills Launch;
-    private float orbitSpeed = 100.0f;
-
-    private List<Skills> skills;
-    
-
     public Transform PlayerPosition;
-    private Rigidbody _rb;
 
+
+    private float orbitSpeed = 100.0f;
+    private int health = 3;
+    private Skills Launch;
+    private List<Skills> skills;
+    private Rigidbody _rb;
     private NavMeshAgent agent;
 
     void Start()
@@ -35,7 +33,7 @@ public class CarinhoScript : MonoBehaviour
     void Update()
     {
 
-        float distance = Vector3.Distance(PlayerPosition.position, transform.position); //Atual distancia entre o carinho e o player
+        //float distance = Vector3.Distance(PlayerPosition.position, transform.position); //Atual distancia entre o carinho e o player
 
         HeartOrbit();
 
@@ -72,6 +70,28 @@ public class CarinhoScript : MonoBehaviour
         for(int i = 0; i <= carinhoHearts.Length - 1; i++)
         {
             carinhoHearts[i].RotateAround(gameObject.transform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+        }
+    }
+
+    void Damage()
+    {
+        health -= 1;
+        carinhoHearts[health].gameObject.SetActive(false);
+        orbitSpeed += 100;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+
+        if(rb != null && collision.collider.CompareTag("rock"))
+        {
+            Damage();
         }
     }
 
