c15440c75f49d0c4a75892d696572eae63288a8d
Arthur Ozorio <arthurlozorio@globo.com>
2020-04-11 20:17:00 -0300

    Orbit agora em um script separado

---
+++ Time03/Assets/Scripts/HeartOrbit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartOrbit : MonoBehaviour
+{
+
+    public float orbitSpeed = 100.0f;
+    public float ProbabilidadeOrbit;
+    public float CooldownOrbit;
+    public Skills Orbit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Orbit = new Skills(ProbabilidadeOrbit, CooldownOrbit, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OrbitAround(Transform[] hearts)
+    {
+        for(int i = 0; i <= hearts.Length - 1; i++)
+        {
+            hearts[i].RotateAround(gameObject.transform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+        }
+    }
+}
