45f2a8e7b1a53a5db2731a3f72a1bd1491a18891
Arthur Ozorio <arthurlozorio@globo.com>
2020-04-12 00:37:25 -0300

    Corações do carinho se expandem e retraem

--- Time03/Assets/Scripts/HeartOrbit.cs
+++ Time03/Assets/Scripts/HeartOrbit.cs
@@ -10,6 +10,14 @@ public class HeartOrbit : MonoBehaviour
     public float CooldownOrbit;
     public Skills Orbit;
 
+    private float radius;
+    public float maxRadius = 10.0f;
+    public float minRadius = 2.0f;
+    public float expansionSpeed;
+    public Transform center;
+    public Vector3 newOrbit;
+    private bool canExpand = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +35,34 @@ public class HeartOrbit : MonoBehaviour
         for(int i = 0; i <= hearts.Length - 1; i++)
         {
             hearts[i].RotateAround(gameObject.transform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+            newOrbit = (hearts[i].position - center.position).normalized * radius + center.position;
+            hearts[i].position = Vector3.MoveTowards(hearts[i].position, newOrbit, Time.deltaTime * expansionSpeed);
+            if(canExpand)
+            {
+                StartCoroutine(Expand());
+            }
+            else
+            {
+                StartCoroutine(Retract());
+            }           
         }
+    }
+
+    public IEnumerator Expand()
+    {
+        radius = maxRadius;     
+
+        yield return new WaitForSeconds(CooldownOrbit);
+
+        canExpand = false;
+    }
+
+    public IEnumerator Retract()
+    {
+        radius = minRadius;
+
+        yield return new WaitForSeconds(CooldownOrbit);
+
+        canExpand = true;
     }
 }
