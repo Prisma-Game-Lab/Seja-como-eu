414de63581a67e75ea52e75063b236966af45d94
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2020-04-13 08:29:39 -0300

    bugfix

--- Time03/Assets/Scripts/BossSkillsCD.cs
+++ Time03/Assets/Scripts/BossSkillsCD.cs
@@ -7,6 +7,14 @@ public class BossSkillsCD : MonoBehaviour
 
     // Retorna a posicao da Skill que vai ser escolhida
     public void ChooseSkill(List<Skills> skills) {
+        int ReturnCount = 0;
+        foreach(Skills s in skills) {
+            if(!s.IsSkillReady())
+                ReturnCount++;
+        }
+        if(ReturnCount == skills.Count)
+            return;
+
         while(true) {
             int id = Random.Range(0,skills.Count);
             if(skills[id].IsSkillReady()) {
