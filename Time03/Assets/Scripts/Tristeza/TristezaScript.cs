b7edc84cf038b821304d28722da7039f9bcd0132
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2020-04-15 21:19:43 -0300

    Um pouco da movimentacao

--- Time03/Assets/Scripts/Tristeza/TristezaScript.cs
+++ Time03/Assets/Scripts/Tristeza/TristezaScript.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TristezaScript : MonoBehaviour
 {
+    public GameObject Player;
     private SadRain srScript;
     private SadPistol spScript;
     private Skills Rain;
@@ -12,9 +14,18 @@ public class TristezaScript : MonoBehaviour
     private BossSkillsCD SkillCD;
     public float Cooldown;
     private bool SkillIsReady = false;
+    private NavMeshAgent Agent;
+    private bool CanGetNewDirection = true;
+
+    private Vector3 Direction;
+
+    public float RunAwayDistance;
 
     void Start()
     {
+
+        Agent = GetComponent<NavMeshAgent>();
+
         skills = new List<Skills>();
 
         srScript = GetComponent<SadRain>();
@@ -22,7 +33,7 @@ public class TristezaScript : MonoBehaviour
         skills.Add(Rain);
 
         spScript = GetComponent<SadPistol>();
-        Pistol = new Skills(spScript.getProb(),spScript.getCD(),true,spScript.Pistol);
+        Pistol = new Skills(spScript.getProb(),spScript.getCD(),false,spScript.Pistol);
         skills.Add(Pistol);
 
         SkillCD = GetComponent<BossSkillsCD>();
@@ -33,15 +44,53 @@ public class TristezaScript : MonoBehaviour
     
     void Update()
     {
+        FleeFromPlayer();
+
         if(SkillIsReady) {
             SkillCD.ChooseSkill(skills);
             StartCoroutine(ResetCooldown());
         }
     }
 
-    IEnumerator ResetCooldown() {
+    private IEnumerator ResetCooldown() {
         SkillIsReady = false;
         yield return new WaitForSeconds(Cooldown);
         SkillIsReady = true;
     }
+
+    private void FleeFromPlayer() {
+        float distance = Vector3.Distance(transform.position,Player.transform.position);
+
+        if(distance < RunAwayDistance) {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position + dirToPlayer;
+
+            Agent.SetDestination(newPos);
+        }
+        else {
+            
+            if(CanGetNewDirection) {
+                Direction = ChooseDirection();
+                StartCoroutine(ChangeDirection());
+            }
+
+            Agent.SetDestination(Direction);
+        }
+    }
+
+    private IEnumerator ChangeDirection() {
+        CanGetNewDirection = false;
+        yield return new WaitForSeconds(8.0f);
+        CanGetNewDirection = true;
+    }
+
+    private Vector3 ChooseDirection() {
+        float xPosition = Random.Range(-19,19);
+        float zPosition = Random.Range(-19,19);
+        Vector3 newPos = new Vector3(xPosition,0,zPosition);
+        Debug.Log(newPos);
+        return newPos;
+    }
+
 }
