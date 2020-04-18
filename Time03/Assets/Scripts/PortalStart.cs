738a4cf6aa80f0cf83a3db022eda121451ea18bb
Sol <solmseda@gmail.com>
2020-04-18 13:42:59 -0300

    Portal para iniciar a batalha do carinho

---
+++ Time03/Assets/Scripts/PortalStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PortalStart : MonoBehaviour
+{
+    public GameObject enemy;
+
+    private NavMeshAgent agent;
+
+    void Start() {
+        agent = enemy.GetComponent<NavMeshAgent>();
+        agent.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+            agent.enabled = true;
+    }
+}
