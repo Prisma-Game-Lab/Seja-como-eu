511842d2bf881c9e3b41198e77b13d9197d6dd57
Arthur Ozorio <arthurlozorio@globo.com>
2020-04-29 22:14:46 -0300

    Início Roll

---
+++ Time03/Assets/Scripts/Tristeza/SadRoll.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SadRoll : MonoBehaviour
+{
+    public float rollWindup;
+
+    public float rollSpeed;
+
+    public float Probabilidade;
+
+    public float Cooldown;
+
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Roll()
+    {
+        float posx = Random.Range(-19, 19);
+        float posz = Random.Range(-19, 19);
+        Vector3 destination = new Vector3(posx, transform.position.y, posz);
+        Debug.Log(destination);
+
+        StartCoroutine(RollDestination(destination));
+    }
+
+    private IEnumerator RollDestination(Vector3 target)
+    {
+        agent.isStopped = true;
+
+        yield return new WaitForSeconds(0.5f);
+
+        transform.LookAt(target);
+
+        yield return new WaitForSeconds(rollWindup);
+
+        while(transform.position != target)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, rollSpeed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+
+        agent.isStopped = false;
+    }
+
+    public float getProb()
+    {
+        return Probabilidade;
+    }
+
+    public float getCD()
+    {
+        return Cooldown;
+    }
+}
