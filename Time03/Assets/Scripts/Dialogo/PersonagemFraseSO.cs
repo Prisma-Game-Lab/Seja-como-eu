86dd39ac9d269f647f14c91063413d470a1482db
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2020-05-06 22:50:56 -0300

    sobras

--- Time03/Assets/Scripts/Dialogo/PersonagemFraseSO.cs
+++ Time03/Assets/Scripts/Dialogo/PersonagemFraseSO.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Frase", menuName = "ScriptableObjects/PersonagemFraseSO", order = 1)]
+[CreateAssetMenu(fileName = "Dialogo", menuName = "ScriptableObjects/PersonagemFraseSO", order = 1)]
 public class PersonagemFraseSO : ScriptableObject
 {
     public List<Dialogue> Frase;
