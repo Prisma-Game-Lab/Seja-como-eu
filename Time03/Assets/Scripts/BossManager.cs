9fef65676ae07c6fb797abf1f7550ea7a4b87450
Arthur Ozorio <arthurlozorio@globo.com>
2020-05-29 19:54:19 -0300

    Não pode mais entrar na fase depois de derrotar o boss

---
+++ Time03/Assets/Scripts/BossManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManager : MonoBehaviour
+{
+    public GameObject PortalCarinho;
+    public GameObject PortalTristeza;
+
+    private GeneralCounts Counts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Counts = SaveSystem.GetInstance().generalCounts;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Counts.CarinhoIsMorto)
+        {
+            PortalCarinho.GetComponent<PortalScript>().CanEnter = false;
+        }
+        if(Counts.TristezaIsMorto)
+        {
+            PortalTristeza.GetComponent<PortalScript>().CanEnter = false;
+        }
+    }
+}
