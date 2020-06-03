e643defbdf6308cc246ccd0b37c0c5b7afa8ae4e
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2020-06-02 21:10:09 -0300

    Dialogo deve estar funcionando

---
+++ Time03/Assets/Scripts/Dialogo/Options.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Options
+{
+    [TextArea(2,3)]
+    public string Texto;
+
+    public int NextChat;
+}
