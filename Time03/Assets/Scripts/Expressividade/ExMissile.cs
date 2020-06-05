23aadb734bbdf1162fd538627afa8c85c57b5113
Arthur Ozorio <arthurlozorio@globo.com>
2020-06-05 20:12:17 -0300

    Início cena expressividade

---
+++ Time03/Assets/Scripts/Expressividade/ExMissile.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExMissile : MonoBehaviour
+{
+
+    public GameObject MissilePrefab;
+    [Range(0,100)]
+    public float probablidade;
+    public float cooldown;
+    public Transform target;
+    public int NumMissiles;
+    public float gap;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MissilePrefab.GetComponent<Missil>().target = target;
+        MissilePrefab.GetComponent<Missil>().speed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Shoot()
+    {
+        StartCoroutine(MissileLaunch());
+    }
+
+    private IEnumerator MissileLaunch()
+    {
+        for(int i = 0; i<NumMissiles;i++)
+        {
+            Instantiate(MissilePrefab, transform.position + new Vector3(0,5,0), Quaternion.identity);
+            yield return new WaitForSeconds(gap);
+        }
+    }
+    public float getProb()
+    {
+        return probablidade;
+    }
+
+    public float getCD()
+    {
+        return cooldown;
+    }
+}
