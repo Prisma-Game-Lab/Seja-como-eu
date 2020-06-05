1afc83ab02a891a70aefd5caeb698fc0d6600a12
Arthur Ozorio <arthurlozorio@globo.com>
2020-06-05 20:56:59 -0300

    Misseis continuam em frente se não acertarem o player

--- Time03/Assets/Scripts/Expressividade/Missil.cs
+++ Time03/Assets/Scripts/Expressividade/Missil.cs
@@ -6,16 +6,38 @@ public class Missil : MonoBehaviour
 {
     public Transform target;
     public float speed;
+    public float autoTime;
+    private bool following = true;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine(Autopilot());
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        if(following)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position + new Vector3(0,0.5f,0), speed * Time.deltaTime);
+        }
+    }
+
+    private IEnumerator Autopilot()
+    {
+        yield return new WaitForSeconds(autoTime);
+
+        following = false;
+        rb.velocity = target.position.normalized * speed;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!collision.collider.CompareTag("orb"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
