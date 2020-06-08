a82df581f241aa44427444695d3eb377656210a3
Sol <solmseda@gmail.com>
2020-06-08 16:54:17 -0300

    Script pro AudioMixer completo

--- Time03/Assets/Scripts/AudioMixerControl.cs
+++ Time03/Assets/Scripts/AudioMixerControl.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioMixerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioMixer mixer;
+
+    public void SetMasterVolume(float sliderValue)
     {
-        
+        mixer.SetFloat("masterVol", Mathf.Log10(sliderValue) * 20);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetMusicVolume (float sliderValue)
     {
-        
+        mixer.SetFloat("musicVol", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetSfxVolume(float sliderValue)
+    {
+        mixer.SetFloat("sfxVol", Mathf.Log10(sliderValue) * 20);
     }
 }
