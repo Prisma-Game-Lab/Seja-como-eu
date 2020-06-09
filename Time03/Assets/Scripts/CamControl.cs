41e65d377db4ccc9d4bee06064c18ed396c2f4d4
Arthur Ozorio <arthurlozorio@globo.com>
2020-06-09 20:44:15 -0300

    Camera hub

---
+++ Time03/Assets/Scripts/CamControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject Camera1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x > 24)
+        {
+            Camera1.gameObject.SetActive(false);
+        }
+        else
+        {
+            Camera1.gameObject.SetActive(true);
+        }
+    }
+}
