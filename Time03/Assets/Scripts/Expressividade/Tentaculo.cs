613d39a016ccdec4bf348427487ac530d6dc4b66
NagibSuaid <nagibmoura@gmail.com>
2020-06-19 19:02:46 -0300

    tentaculos da expressividade

---
+++ Time03/Assets/Scripts/Expressividade/Tentaculo.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tentaculo : MonoBehaviour
+{
+	public GameObject tentaculo;
+	public int gapSize = 3;
+	public float gapPeriod = 4;
+	public float speed = 5;
+	public float despawnDuration = 5;
+	private int gaps = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnTentacle",1.0f,1.0f);
+        StartCoroutine(AddGap());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void spawnTentacle()
+    {
+    	 GameObject t;
+    	 if(gaps>0)
+    	 {
+    	 	gaps=gaps - 1;
+    	 }
+    	 else
+    	 {
+    	 	t = Instantiate(tentaculo,this.transform);
+    	 	t.GetComponent<Rigidbody>().AddForce(transform.forward * speed * 10);
+    	 	StartCoroutine(Despawn(t));
+    	 }
+
+
+    }
+
+    private IEnumerator AddGap()
+    {
+    	yield return new WaitForSeconds(1.0f);
+    	while (true)
+    	{
+	    	yield return new WaitForSeconds(Random.Range(0.0f,2.0f));
+	    	gaps += gapSize;
+	    	yield return new WaitForSeconds(gapPeriod);
+    	}
+    }
+
+    private IEnumerator Despawn(GameObject obj)
+    {
+    	yield return new WaitForSeconds(despawnDuration);
+        Destroy(obj);
+    }
+}
+    
+
