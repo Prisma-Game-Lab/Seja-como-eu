53573b788436fc1bbc452f920c191cd30f67d4a1
NagibSuaid <nagibmoura@gmail.com>
2020-07-09 22:50:17 -0300

    Tutorial Expressividade

---
+++ Time03/Assets/Scripts/Expressividade/HitTentacleTutorial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitTentacleTutorial : MonoBehaviour
+{
+    private MovimentPlayer _mp;
+
+    void Start()
+    {
+        _mp = GetComponent<MovimentPlayer>();
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.collider.CompareTag("VulnTent"))
+        {
+            if(_mp.dashing)
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
