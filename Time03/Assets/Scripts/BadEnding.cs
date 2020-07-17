5c3e3d5e92dafabf2a0760d7507c144bf92b2a2f
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2020-07-16 21:27:15 -0300

    Dialogo Final Bad Ending

---
+++ Time03/Assets/Scripts/BadEnding.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadEnding : MonoBehaviour
+{
+    
+    public GameObject EndingCanvas;
+    public GameObject Player;
+
+    private GeneralCounts Counts;
+
+    void Start()
+    {
+        Counts = SaveSystem.GetInstance().generalCounts;
+    }
+    
+    void Update()
+    {
+        if(Counts.Index == 110) {
+            EndingCanvas.SetActive(true);
+            Player.GetComponent<MovimentPlayer>().enabled = false;
+            GetComponent<GameManager>().canPause = false;
+            Counts.Events["DialogoFinal"] = true;
+        }
+    }
+}
