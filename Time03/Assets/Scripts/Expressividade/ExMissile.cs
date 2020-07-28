7fdd2ed1ec442fb41670efee4bc5c86869d826bd
pedro <pferndd@gmail.com>
2020-07-27 23:25:36 -0300

    expressividade missil edit

--- Time03/Assets/Scripts/Expressividade/ExMissile.cs
+++ Time03/Assets/Scripts/Expressividade/ExMissile.cs
@@ -10,6 +10,7 @@ public class ExMissile : MonoBehaviour
     public float probablidade;
     public float cooldown;
     public Transform target;
+    public Transform targetInit;
     public int NumMissiles;
     public float gap;
     public float speed;
@@ -20,6 +21,7 @@ public class ExMissile : MonoBehaviour
     void Start()
     {
         MissilePrefab.GetComponent<Missil>().target = target;
+        MissilePrefab.GetComponent<Missil>().targetInit = targetInit;
         MissilePrefab.GetComponent<Missil>().speed = speed;
         MissilePrefab.GetComponent<Missil>().Expressividade = Expressividade;
         headAnim = GetComponentInChildren<Animator>();
@@ -41,7 +43,7 @@ public class ExMissile : MonoBehaviour
     {
         for(int i = 0; i<NumMissiles;i++)
         {
-            Instantiate(MissilePrefab, transform.position + new Vector3(0,5,0), Quaternion.identity);
+            Instantiate(MissilePrefab, transform.position + new Vector3(0,5.5f,-3), Quaternion.identity);
             yield return new WaitForSeconds(gap);
         }
     }
