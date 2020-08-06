9bb8c31e0fdc985149bafbd09ced32f3b2de3c44
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2020-08-06 20:34:28 -0300

    Stats

--- Time03/Assets/Scripts/ShowStats.cs
+++ Time03/Assets/Scripts/ShowStats.cs
@@ -42,47 +42,47 @@ public class ShowStats : MonoBehaviour
         Counts.Stats["ExpressividadeDeathCount"];
         Time.timeScale = 0f;
         transform.GetChild(0).gameObject.SetActive(true);
-        StatsText.text = $"Total Roll Count . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {TotalDashs}\n\nTotal Death Count . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {TotalDeaths}\n\n";
+        StatsText.text = $"Total Roll Count . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {TotalDashs}\n\nTotal Death Count . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {TotalDeaths}\n\n";
 
-        StatsText.text += $"Hub Roll Count . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["HubDashCount"]}\n\n";
+        StatsText.text += $"Hub Roll Count . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["HubDashCount"]}\n\n";
 
-        StatsText.text += Counts.CarinhoIsMorto?$"Affection's Battle Time . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {ConvertToTime(Counts.CarinhoCompleteTimer)}\n\n"
+        StatsText.text += Counts.CarinhoIsMorto?$"Affection's Battle Time . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {ConvertToTime(Counts.CarinhoCompleteTimer)}\n\n"
         :"Affection Has Not Been Defeated.\n\n";
 
-        StatsText.text += Counts.CarinhoIsMorto?$"Affection's Battle Roll Count . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["CarinhoDashCount"]}\n\n"
+        StatsText.text += Counts.CarinhoIsMorto?$"Affection's Battle Roll Count . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["CarinhoDashCount"]}\n\n"
         :"";
 
-        StatsText.text += Counts.CarinhoIsMorto?$"Death by Affection . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["CarinhoDeathCount"]}\n\n"
+        StatsText.text += Counts.CarinhoIsMorto?$"Death by Affection . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["CarinhoDeathCount"]}\n\n"
         :"";
 
-        StatsText.text += Counts.TristezaIsMorto?$"Depression's Battle Time . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {ConvertToTime(Counts.TristezaCompleteTimer)}\n\n"
+        StatsText.text += Counts.TristezaIsMorto?$"Depression's Battle Time . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {ConvertToTime(Counts.TristezaCompleteTimer)}\n\n"
         :"Depression Has Not Been Defeated.\n\n";
 
-        StatsText.text += Counts.TristezaIsMorto?$"Depression's Battle Roll Count . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["TristezaDashCount"]}\n\n"
+        StatsText.text += Counts.TristezaIsMorto?$"Depression's Battle Roll Count . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["TristezaDashCount"]}\n\n"
         :"";
 
-        StatsText.text += Counts.TristezaIsMorto?$"Death by Depression . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["TristezaDeathCount"]}\n\n"
+        StatsText.text += Counts.TristezaIsMorto?$"Death by Depression . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["TristezaDeathCount"]}\n\n"
         :"";
 
-        StatsText.text += Counts.ExpressividadeIsMorto?$"Expression's Battle Time . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {ConvertToTime(Counts.ExpressividadeCompleteTimer)}\n\n"
+        StatsText.text += Counts.ExpressividadeIsMorto?$"Expression's Battle Time . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {ConvertToTime(Counts.ExpressividadeCompleteTimer)}\n\n"
         :"Expression Has Not Been Defeated.\n\n";
 
-        StatsText.text += Counts.ExpressividadeIsMorto?$"Expression's Battle Roll Count . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["ExpressividadeDashCount"]}\n\n"
+        StatsText.text += Counts.ExpressividadeIsMorto?$"Expression's Battle Roll Count . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["ExpressividadeDashCount"]}\n\n"
         :"";
 
-        StatsText.text += Counts.ExpressividadeIsMorto?$"Death by Expression . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["ExpressividadeDeathCount"]}\n\n"
+        StatsText.text += Counts.ExpressividadeIsMorto?$"Death by Expression . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["ExpressividadeDeathCount"]}\n\n"
         :"";
 
-        StatsText.text += Counts.MDMIsMorto?$"Master Macho's Battle Time . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {ConvertToTime(Counts.MDMCompleteTimer)}\n\n"
+        StatsText.text += Counts.MDMIsMorto?$"Master Macho's Battle Time . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {ConvertToTime(Counts.MDMCompleteTimer)}\n\n"
         :"????????????????????????????\n\n";
 
-        StatsText.text += Counts.MDMIsMorto?$"Master Macho's Battle Roll Count . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["MDMDashCount"]}\n\n"
+        StatsText.text += Counts.MDMIsMorto?$"Master Macho's Battle Roll Count . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["MDMDashCount"]}\n\n"
         :"";
 
-        StatsText.text += Counts.MDMIsMorto?$"Death by Toxic Masculinity . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["MDMDeathCount"]}\n\n"
+        StatsText.text += Counts.MDMIsMorto?$"Death by Toxic Masculinity . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {Counts.Stats["MDMDeathCount"]}\n\n"
         :"";
 
-        StatsText.text += $"Total Play Time . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {ConvertToTime(Counts.TotalPlayTime)}";
+        StatsText.text += $"Total Play Time . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . {ConvertToTime(Counts.TotalPlayTime)}";
     }
 
     private string ConvertToTime(float time) {
